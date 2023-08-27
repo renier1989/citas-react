da7c9be9640705a09a70e541b26fb9ebaf15b07c
Renier Vargas <renier1989@gmail.com>
2023-08-27 11:34:36 -0500

    Class 59 - 60 - Usando las clases de Tailwind y aplicando estilos al Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Formulario />
-      <ListadoPacientes />
+      <div className="container mx-auto mt-20">
+        <Header />
+        <Formulario />
+        <ListadoPacientes />
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+      Seguimientos Pacientes {""}
+      <span className="text-indigo-600">Veterinaria</span>
+    </h1>
   );
 }
 
